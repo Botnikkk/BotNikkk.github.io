aae1e8b193dbf6ccdcb62f91ddfff58a6f48f8e2
Nikhil Chaudhary <Nikhil.ace.chd@gmail.com>
2024-05-09 16:21:03 +0530

    animation removed

--- scripts.js
+++ scripts.js
@@ -1,29 +1,3 @@
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show');
-        }
-
-   });
-});
-
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
-
-
-const observer2 = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting) {
-            entry.target.classList.add('projects-show');
-        }
-   });
-});
-
-const hiddenElements2 = document.querySelectorAll('.projects-hidden');
-hiddenElements2.forEach((el) => observer2.observe(el));
-
 const mobile_nav = document.querySelector('.mobile-icon');
 const nav_header = document.querySelector('.header');
 const something = document.querySelectorAll('.nav-icon')
